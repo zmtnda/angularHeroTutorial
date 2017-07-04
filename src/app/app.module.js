4e11c710736261a9dcdaae554d67884dc5506ab0
Zin Tun (zitun) <zitun@cisco.com>
2017-07-04 09:58:04 -0700

    changes in app.module.ts

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,3 +1,7 @@
+//import { NgModule }      from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { FormsModule }   from '@angular/forms';
+//import { AppComponent }  from './app.component';
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -6,8 +10,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var animations_1 = require("@angular/platform-browser/animations");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
+var app_routing_module_1 = require("./app-routing.module");
+var ngmaterial_module_1 = require("./ngmaterial.module");
+var home_module_1 = require("./home/home.module");
 var app_component_1 = require("./app.component");
 var AppModule = (function () {
     function AppModule() {
@@ -16,7 +24,10 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
+        imports: [platform_browser_1.BrowserModule, forms_1.FormsModule,
+            animations_1.BrowserAnimationsModule, app_routing_module_1.AppRoutingModule,
+            ngmaterial_module_1.AngularMaterialModule,
+            home_module_1.HomeModule],
         declarations: [app_component_1.AppComponent],
         bootstrap: [app_component_1.AppComponent]
     })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,23 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+//import { NgModule }      from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { FormsModule }   from '@angular/forms';
+//import { AppComponent }  from './app.component';
+
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AngularMaterialModule} from './ngmaterial.module';
+import {HomeModule} from './home/home.module';
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,
+                  BrowserAnimationsModule, AppRoutingModule,
+                  AngularMaterialModule,
+                  HomeModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
